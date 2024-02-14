945471663424a3502ffdc1f4322d60aad1b9832a
karlorz <karl.logon@gmail.com>
2024-02-14 23:53:08 +0800

    fix auth v5

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,24 @@
+import { auth } from 'auth';
+import Link from 'next/link';
+
+import { SignInButton } from '@/components/navbar/sign-in-button';
+import { ThemeToggle } from '@/components/navbar/theme-toggle';
+import { UserDropdown } from '@/components/navbar/user-dropdown';
+
+export const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <header className="w-full border-b">
+      <div className="container flex h-16 items-center justify-between">
+        <Link href="/" className="font-mono text-lg font-bold">
+          next-starter
+        </Link>
+        <div className="flex items-center gap-2">
+          <ThemeToggle />
+          {session ? <UserDropdown session={session} /> : <SignInButton />}
+        </div>
+      </div>
+    </header>
+  );
+};
