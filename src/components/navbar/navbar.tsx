975e62fb8cb708ce63ef41777cc8c0fef69148c8
karlorz <karl.logon@gmail.com>
2024-02-15 00:20:43 +0800

    update

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,7 +3,8 @@ import Link from 'next/link';
 
 import { SignInButton } from '@/components/navbar/sign-in-button';
 import { ThemeToggle } from '@/components/navbar/theme-toggle';
-import { UserDropdown } from '@/components/navbar/user-dropdown';
+// import { UserDropdown } from '@/components/navbar/user-dropdown';
+import UserButton from '@/components/user-button';
 
 export const Navbar = async () => {
   const session = await auth();
@@ -16,7 +17,7 @@ export const Navbar = async () => {
         </Link>
         <div className="flex items-center gap-2">
           <ThemeToggle />
-          {session ? <UserDropdown session={session} /> : <SignInButton />}
+          {session ? <UserButton /> : <SignInButton />}
         </div>
       </div>
     </header>
