8208b4bfed564ee6e1fc827425ac740ee957917a
karlorz <karl.logon@gmail.com>
2024-02-15 22:25:30 +0800

    save begin test

--- src/__tests__/e2e/begin.spec.ts
+++ src/__tests__/e2e/begin.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test';
 import { siteConfig } from '@/lib/constant';
 
 test('has title', async ({ page }) => {
-  await page.goto('/test_begin');
+  await page.goto('/starter');
 
   await expect(page).toHaveTitle(siteConfig.title);
 });
