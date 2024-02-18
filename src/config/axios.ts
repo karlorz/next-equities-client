24619cb05e0ece6a1c7de8c1a88480fa9400bac0
karlorz <karl.logon@gmail.com>
2024-02-18 21:32:39 +0800

    apiurl

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: 'http://ec2-54-255-191-34.ap-southeast-1.compute.amazonaws.com:8080',
 });
 
 export default instance;
