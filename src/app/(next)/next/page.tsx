3491e425e19e763e128c8871ecdcddd0368c449a
karlorz <karl.logon@gmail.com>
2024-02-18 22:58:55 +0800

    add test api

--- src/app/(next)/next/page.tsx
+++ src/app/(next)/next/page.tsx
@@ -1,37 +1,45 @@
 /* eslint-disable react/no-unescaped-entities */
 'use client';
-import { useSession } from 'next-auth/react';
+// import { useSession } from 'next-auth/react';
 
 import { HeroForm } from '@/components/form';
 import { SignOutButton } from '@/components/sign-out-button';
 import { env } from '@/env.mjs';
+import useQueryTest from '@/hooks/useQueryTest';
 
-// const env.NEXT_PUBLIC_API_URL;
+function Next() {
+  // const { data: session } = useSession();
+  const { data: watchlist, isLoading, isError } = useQueryTest();
 
-const Next = () => {
-  const { data: session } = useSession();
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error fetching data</div>;
+  }
 
   return (
     <section>
+      <p>{JSON.stringify(watchlist)}</p>
       <p>{env.NEXT_PUBLIC_API_URL}</p>
-      <p>{process.env.NEXT_PUBLIC_API_URL}</p>
-      {session && (
-        <div>
-          <h1>next-equities</h1>
-          {session.user && (
-            <>
-              <p>Session user:</p>
-              <p>Name: {session.user.name}</p>
-              <p>Email: {session.user.email}</p>
-              <p>Image: {session.user.image}</p>
-            </>
-          )}
-        </div>
-      )}
+      {/* {session && (
+              <div>
+                <h1>next-equities</h1>
+                {session.user && (
+                  <>
+                    <p>Session user:</p>
+                    <p>Name: {session.user.name}</p>
+                    <p>Email: {session.user.email}</p>
+                    <p>Image: {session.user.image}</p>
+                  </>
+                )}
+              </div>
+            )} */}
       <HeroForm />
       <SignOutButton />
     </section>
   );
-};
+}
 
 export default Next;
--- src/hooks/useQueryTest.ts
+++ src/hooks/useQueryTest.ts
@@ -1,23 +1,23 @@
 import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
-import axios from '@/config/axios';
 import { Watchlist } from '@/types/watchlist/watachlist';
 
-export const QUERY_WATCHLIST = 'QUERY_WATCHLIST';
+export const QUERY_TEST = 'QUERY_TEST';
 
 const queryWatchlist = async () => {
   // const response = await axios.get<{
   //   data: WatchlistDto[];
   // }>('/api/v1/watchlists');
   // return response.data?.data?.map((watachlistDto) => toModel(watachlistDto));
-  const response = await axios.get('/api/v1/watchlists');
+  const response = await axios.get(`${process.env.API_PATH}/v1/watchlists`);
   const watchlists = response.data as Watchlist[];
   return watchlists;
 };
 
 const useQueryTest = () => {
   return useQuery({
-    queryKey: [QUERY_WATCHLIST],
+    queryKey: [QUERY_TEST],
     queryFn: queryWatchlist,
   });
 };
