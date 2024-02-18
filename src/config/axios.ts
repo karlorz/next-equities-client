0f0022c8dcf97893b095fd91c7288ea9b57e78a3
karlorz <karl.logon@gmail.com>
2024-02-18 21:27:18 +0800

    api url

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,15 +1,7 @@
 import axios from 'axios';
 
-// import { env } from '@/env.mjs';
-
 const instance = axios.create({
-  baseURL: (() => {
-    const publicApiUrl = process.env.NEXT_PUBLIC_API_URL;
-    if (!publicApiUrl) {
-      return process.env.NEXT_PUBLIC_API_URL;
-    }
-    return publicApiUrl;
-  })(),
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export default instance;
